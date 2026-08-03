b3c343e0842c4468b79c0796d807400797bb157d
amiosamu <84852496+amiosamu@users.noreply.github.com>
2026-08-03 20:23:15 +0500

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        candidate1 = candidate2 = None
+        count1 = count2 = 0
+
+        # Find potential candidates
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        # Verify candidates
+        count1 = count2 = 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+
+        res = []
+        n = len(nums)
+        if count1 > n // 3:
+            res.append(candidate1)
+        if count2 > n // 3:
+            res.append(candidate2)
+
+        return res
